172b3e62873dbad44db60d29c810bf02b42cbf63
Lucas Primo <lucaslprimo@gmail.com>
2024-10-20 19:27:58 -0300

    Spawning Enemies
    - Enemies are spawning randomly, including planets

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+@export var enemies:Array[PackedScene]  = []
+
 @onready var player = $Player
 @onready var player_spawn_point = $PlayerSpawnPoint
 @onready var bullet_container = $BulletContainer
+@onready var enemy_container = $EnemyContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,3 +24,10 @@ func on_player_shoot(bullet_scene, location):
 	bullet.global_position = location
 	bullet_container.add_child(bullet)
 	
+func spawn_random_enemy():
+	var enemy = enemies.pick_random().instantiate()
+	enemy.global_position = Vector2(1940, randi_range(0, 1080))
+	enemy_container.add_child(enemy)
+
+func _on_timer_timeout() -> void:
+	spawn_random_enemy()
